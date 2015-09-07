7e87c97ac4477e97618412827acc3f5164a6b8bd
Nathan Chapman <nathan@nchp.mn>
2015-09-07 18:22:05 +1000

    Add navbar structure

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -10,9 +10,31 @@
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
   <?php echo css(array(
-    'assets/css/bootstrap.css',
+    'modules/bootstrap/css/bootstrap.css',
     'assets/css/main.css'
   )) ?>
 
 </head>
 <body>
+
+<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+  <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#topNavbar">
+    &#9776;
+  </button>
+  <div class="container collapse navbar-toggleable-xs" id="topNavbar">
+    <ul class="nav navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#work">Work</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#blog">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
