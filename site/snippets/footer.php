840aeec9113d15099f1a6f99ee4c6e51753f6061
Nathan Chapman <nathan@nchp.mn>
2015-09-07 18:29:47 +1000

    Add footer structure

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,14 +1,15 @@
-  <footer class="footer cf" role="contentinfo">
-
-    <div class="copyright">
-      <?php echo $site->copyright()->kirbytext() ?>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-4 col-xs-offset-2 copyright">
+        <?php echo $site->copyright()->kirbytext() ?>
+      </div>
+      <div class="col-xs-4 colophon text-right">
+        Hint: Take the lens cap off.
+      </div>
     </div>
-
-    <div class="colophon">
-      <a href="http://getkirby.com/made-with-kirby-and-love">Made with Kirby and <b>♥</b></a>
-    </div>
-
-  </footer>
+  </div
+</footer>
 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
