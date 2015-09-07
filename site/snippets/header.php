9497f51f0aa695be98bc678d6396bc1dad59bde1
Nathan Chapman <nathan@nchp.mn>
2015-09-07 15:04:57 +1000

    Add bootstrap.css linked file

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,7 +9,10 @@
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
-  <?php echo css('assets/css/main.css') ?>
+  <?php echo css(array(
+    'assets/css/bootstrap.css',
+    'assets/css/main.css'
+  )) ?>
 
 </head>
 <body>
